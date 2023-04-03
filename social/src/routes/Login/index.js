0fb548b0b421147bf87d4e9cb81888036d90d478
wwl-jky <jky@winwinlab.co.kr>
2023-04-03 15:34:39 +0900

    google_login

--- social/src/routes/Login/index.js
+++ social/src/routes/Login/index.js
@@ -1,6 +1,8 @@
 
 import { useEffect } from "react";
-import { REST_API_KEY , REDIRECT_URI , CLIENT_KEY } from "../../data/keyData";
+import { REST_API_KEY , REDIRECT_URI , CLIENT_KEY , CLIENT_ID} from "../../data/keyData";
+
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 
 const Login = () => {
     // 카카오 로그인
@@ -18,7 +20,7 @@ const Login = () => {
             clientId: CLIENT_KEY,
             callbackUrl : REDIRECT_URI,
             isPopup: false,
-            loginButton: {color: 'white' , type: 1, height: '47'},
+            loginButton: {color: 'green' , type: 3, height: '40'},
             callbackHandle: true
         });
 
@@ -26,18 +28,29 @@ const Login = () => {
     }
 
     useEffect(() => {
-        handleNaverLogin();
+        handleNaverLogin();  //네이버 로그인
     } , [])
 
     return (
         <div>
             <div id="kakaoIdLogin">
                 <a id="kakao-login-btn" onClick={handleKakaoLogin}>
-                    <img src="https://k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg" width="222" alt="카카오 로그인 버튼" />
+                    <img src="https://k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg" alt="카카오 로그인 버튼" />
                 </a>
             </div>
 
             <div id='naverIdLogin'></div>
+
+            <GoogleOAuthProvider clientId={`${CLIENT_ID}`}>
+                <GoogleLogin
+                    buttonText="구글로 계속하기"
+                    clientId={`${CLIENT_ID}`}
+                    onSuccess={(res) => console.log(res)}
+                    onFailure={(res) => console.log(res)}
+                />
+            </GoogleOAuthProvider>
+
+
         </div>
     )
 }
